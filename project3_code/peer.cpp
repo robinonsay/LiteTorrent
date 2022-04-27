c96400800b8ad2af91748d6f88b090fe2b123f3c
Robin Onsay <hello@robinonsay.com>
2022-04-27 13:44:53 -0400

    Peer takes user input

--- project3_code/peer.cpp
+++ project3_code/peer.cpp
@@ -4,19 +4,20 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MIN_ARGS 5
+#define MIN_ARGS 7
 
 int main(int argc, char *argv[]){
 //    struct sockaddr_in server_addr;
 //    socklen_t addrlen = sizeof(server_addr);
 //    int status, sockfd, receiverPort; 
-//    char *receiverIP;
+    char *myIP;
+    char *trackerIP;
+    std::string inFilePath;
+    std::ifstream *owndChunksFile;
+    std::ofstream *outFile;
     std::ofstream *log;
-    std::ifstream *peersList;
-    std::ifstream *inFile;
-    std::ifstream *tFile;
     if(argc != MIN_ARGS){
-        printf("Invalid Arguments\nUsage:\n./tracker <peers-list> <input-file> <torrent-file> <log>\n");
+        printf("Invalid Arguments\nUsage:\n./peer <my-ip> <tracker-ip> <input-file> <owned-chunks> <output-file> <log>\n");
         exit(1);
     }
 //    // create UDP socket
@@ -30,20 +31,20 @@ int main(int argc, char *argv[]){
 //        fprintf(stderr, "ERROR invalid IP address\n");
 //        exit(1);
 //    }
-    peersList = new std::ifstream(argv[1]);
-    inFile = new std::ifstream(argv[2], std::ifstream::binary);
-    tFile = new std::ifstream(argv[3]);
-    log = new std::ofstream(argv[4], std::ofstream::app);
-    if(!peersList->is_open()) printf("Peers list file does not exist\n");
-    if(!inFile->is_open()) printf("Input file does not exist\n");
-    if(!tFile->is_open()) printf("Torrent file does not exist\n");
-    peersList->close();
-    inFile->close();
-    tFile->close();
+    myIP = argv[1];
+    trackerIP = argv[2];
+    inFilePath = std::string(argv[3]);
+    owndChunksFile = new std::ifstream(argv[4]);
+    outFile = new std::ofstream(argv[5]);
+    log = new std::ofstream(argv[6], std::ofstream::app);
+    if(!owndChunksFile->is_open()) printf("Owned Chunks file does not exist\n");
+    if(!outFile->is_open()) printf("Output file could not be opened. Check path\n");
+    if(!log->is_open()) printf("Log file could not be opened. Check path\n");
+    owndChunksFile->close();
+    outFile->close();
     log->close();
-    delete peersList;
-    delete inFile;
-    delete tFile;
+    delete owndChunksFile;
+    delete outFile;
     delete log;
     return 0;
 }
