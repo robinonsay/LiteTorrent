6331018358fe643ad12011842a3c74483f151af0
Robin Onsay <hello@robinonsay.com>
2022-05-05 11:19:35 -0400

    Adds comments to argparse

--- include/argparse.h
+++ include/argparse.h
@@ -11,15 +11,46 @@ typedef std::list<std::string> ArgList;
 
 class ArgParse{
     public:
+        /**
+        * ArgParse parses the given command line arguments given that all command line arguments provided are required
+        * @param argc argument count
+        * @param argv argument list
+        * @param il initializer list for required arguments
+        */
         ArgParse(int argc, char *argv[], std::initializer_list<std::string> il);
+        /** ArgParse destructor
+        */
         ~ArgParse();
+        /**
+        * Gets help message
+        * @return help message
+        */
         std::string getHelp();
+        /**
+        * Parse command line arguments
+        * @return Mapping of required arguments to command line arguments
+        */
         ArgMap& parseArgs();
     private:
+        /**
+        * Help message
+        */
         std::string helpMsg;
+        /**
+        * Argument map
+        */
         ArgMap args;
+        /**
+        * Required args list
+        */
         ArgList order;
+        /**
+        * Argument count
+        */
         int argc;
+        /**
+        * Argument list
+        */
         char **argv;
 };
 
--- src/argparse.cpp
+++ src/argparse.cpp
@@ -11,14 +11,19 @@
 #include <initializer_list>
 
 ArgParse::ArgParse(int argc, char *argv[], std::initializer_list<std::string> il){
+    // Set order to initializer list
     this->order = il;
+    // Argument list iterator
     ArgList::iterator it;
+    // String stream for help message
     std::ostringstream oss;
+    // Build help message
     oss << "Usage:" << std::endl;
     oss << argv[0];
     for(it = this->order.begin(); it != this->order.end(); ++it){
         oss << " <" << *it << ">";
     }
+    // Set help message
     this->helpMsg = oss.str();
     this->argc = argc;
     this->argv = argv;
@@ -28,21 +33,25 @@ ArgParse::~ArgParse(){
 }
 
 std::string ArgParse::getHelp(){
+    // return help message
     return this->helpMsg;
 }
 
 ArgMap& ArgParse::parseArgs(){
     ArgList::iterator it;
     int i = 1;
+    // Check if number of arguments was passed
     if(this->order.size() != (size_t) this->argc - 1){
         error(std::cerr, "Invalid Argument(s)");
         info(std::cout, this->helpMsg.c_str());
         exit(1);
     }
+    // Set arguments to map
     for(it = this->order.begin(); it != this->order.end() && i < this->argc; ++it){
         this->args[*it] = this->argv[i];
         i++;
     }
+    // Return map by reference
     return this->args;
 }
 
